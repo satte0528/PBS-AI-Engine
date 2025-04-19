9db2b12db9a411e5a5fa66705ddb784fd5362f87
rohith <35054605+mullapudirohith@users.noreply.github.com>
2025-04-19 14:18:26 -0400

    CORS error resolution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.routers.resume import router as resume_router
 
 app = FastAPI(title=settings.app_name)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # mount the resume router
 app.include_router(resume_router)
